5af35340cc8ea191f83284e4403fe0c27d2613b1
Ooyoshi-Miki <149459796+Ooyoshi-Miki@users.noreply.github.com>
2023-12-09 11:20:24 +0900

    kadai_005ディレクトリにbmi.jsをアップロード

---
+++ kadai-005/bmi.js
@@ -0,0 +1,26 @@
+// 四則演算
+console.log(68 / (1.7 * 1.7));
+
+
+
+// 変数の宣言・値の代入
+x = 68
+y = (1.7 * 1.7);
+
+// コンソールへの出力
+console.log(x / y);
+
+
+
+//変数の宣言
+let weight;
+let height;
+let bmi = (weight / (height * height));
+
+//値の代入
+weight = 68;
+height = 1.7;
+bmi = weight / (height * height);
+
+//コンソールへの出力
+console.log(bmi);
