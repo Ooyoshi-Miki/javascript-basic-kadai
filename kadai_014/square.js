4910198f91deff2e8fecb224375561a2521f9290
Ooyoshi-Miki <149459796+Ooyoshi-Miki@users.noreply.github.com>
2023-12-17 12:16:20 +0900

    kadai_014ディレクトリにsquare.jsをアップロード

---
+++ kadai_014/square.js
@@ -0,0 +1,19 @@
+// 与えられた引数numを2乗にし、その値を戻り値として返す関数を定義する
+const double = (num) => {
+    return num ** 2;
+}
+// 関数の戻り値を出力する
+console.log(double(10));
+
+
+
+
+// 2乗して答えを100にする式を、functionを使って作る
+function square(x, y) {
+    console.log(x ** y);
+}
+// 関数の呼び出(出力)方は、定数名();  
+square(10, 2);
+
+
+
